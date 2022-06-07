245694a5163981662e6c3591de8945d41af03daa
didi <onoriodedafetta@gmail.com>
2022-06-07 12:24:11 +0100

    first version of leads tracker chrome extension

---
+++ script.js
@@ -0,0 +1,56 @@
+//chrome://extensions/
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const saveBtn = document.getElementById("save-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("del-btn");
+
+// localStorage.setItem("myLeads", "www.google.com")
+
+
+//this is to parse the leads into an array and then store it in the local storage of the browser
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem(myLeads));
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+function render(leads) {
+    let listItems = "";
+
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a href = '${leads[i]}' target = '_blank'>${leads[i]}</a>
+            </li>
+    `       
+    }
+    
+    ulEl.innerHTML = listItems
+}
+
+saveBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+
+    //this is to store items, parsed as strings using JSON in the localStorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+    render(myLeads);
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads)
+})
+
+
+/*to create a list item on the ul tag, here are two ways;
+- innerHTML =>  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+- create element => * const li = document.createElement("li")
+    set textContent * li.textContent = myLeads[i]
+    append to ul    * ulEl.append(li)  */
+
+
